45d091d1c7a8ac099f3e4044e2bc82869f4cf909
zakapong <zaka.pong@yahoo.com>
2016-07-12 08:57:37 +0200

    add todos

--- server.js
+++ server.js
@@ -1,11 +1,54 @@
 var express = require('express');
 var app= express();
 var PORT= process.env.PORT || 3000;
+var todos= [{
+
+id: 1,
+description: 'Meet mom for lunch',
+completed: false
+}, {
+	id: 2,
+	description: 'Go to market',
+	completed: false
+	}, {
+	id: 3,
+	description: 'Feed to cat',
+	completed: true
+
+}];
 
 app.get('/', function (req, res){
 res.send('Todo API Root');
 
+
 });
+//GEt todo
+app.get('/todos', function(req, res){
+	res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchTodo;
+	todos.forEach(function(todo){
+     if(todoId== todo.id){
+
+     	matchTodo= todo;
+     }
+
+	});
+
+	if(matchTodo){
+
+		res.json(matchTodo);
+	}else{
+
+		res.status(404).send();
+	}
+
+	res.send('Asking for todo with the id of ' +req.params.id+ ' !')
+});
+
 
 app.listen(PORT, function(){
 
