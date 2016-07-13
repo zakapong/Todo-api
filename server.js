188c7671e4c149241a3c2d0f32fc28459b95b11d
zakapong <zaka.pong@yahoo.com>
2016-07-13 10:36:06 +0200

    add post for todos

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 var express = require('express');
+var bodyParser= require('body-parser');
+
 var app= express();
 var PORT= process.env.PORT || 3000;
-var todos= [{
+ var todos= [];
+ var todoNextId= 1;
 
-id: 1,
-description: 'Meet mom for lunch oh no',
-completed: false
-}, {
-	id: 2,
-	description: 'Go to market heroku',
-	completed: false
-	}, {
-	id: 3,
-	description: 'Feed to cat',
-	completed: true
+app.use(bodyParser.json());
 
-}];
+ //{
+// id: 1,
+// description: 'Meet mom for lunch oh no',
+// completed: false
+// }, {
+// 	id: 2,
+// 	description: 'Go to market heroku',
+// 	completed: false
+// 	}, {
+// 	id: 3,
+// 	description: 'Feed to cat',
+// 	completed: true
+
+// }
+
 
 app.get('/', function (req, res){
 res.send('Todo API Root');
@@ -49,7 +56,19 @@ app.get('/todos/:id', function (req, res){
 	res.send('Asking for todo with the id of ' +req.params.id+ ' !')
 });
 
+// post
 
+app.post('/todos', function(req, res){
+var body = req.body;
+body.id= todoNextId++;
+todos.push(body);
+
+
+
+//console.log('description ' + body.description);
+res.json(body);
+
+});
 app.listen(PORT, function(){
 
 console.log('Express listening on port' +PORT+ '');
