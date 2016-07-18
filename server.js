89029573d73364db1bbf740f0a2d0a025fb879f4
zakapong <zaka.pong@yahoo.com>
2016-07-18 11:17:34 +0200

    refactor api with underscore

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser= require('body-parser');
-
+var _=require('underscore');
 var app= express();
 var PORT= process.env.PORT || 3000;
  var todos= [];
@@ -36,14 +36,14 @@ app.get('/todos', function(req, res){
 
 app.get('/todos/:id', function (req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchTodo;
-	todos.forEach(function(todo){
-     if(todoId== todo.id){
+	var matchTodo=_.findWhere(todos, {id: todoId});
+	// todos.forEach(function(todo){
+ //     if(todoId== todo.id){
 
-     	matchTodo= todo;
-     }
+ //     	matchTodo= todo;
+ //     }
 
-	});
+	// });
 
 	if(matchTodo){
 
@@ -59,11 +59,17 @@ app.get('/todos/:id', function (req, res){
 // post
 
 app.post('/todos', function(req, res){
-var body = req.body;
+var body = _.pick(req.body, 'description', 'completed');
+
+if(!_.isBoolean(body.completed) || _.isString(body.description)|| body.description.trim().length===0){
+
+	return res.status(400).send();
+}
+
+
+body.description= body.description.trim();
 body.id= todoNextId++;
 todos.push(body);
-
-
 
 //console.log('description ' + body.description);
 res.json(body);
