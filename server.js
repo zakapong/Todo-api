dddd29666f14fe37c9ea696bd5bb7df8aeb7457f
zakapong <zaka.pong@yahoo.com>
2016-07-18 13:04:57 +0200

    put request implementation

--- server.js
+++ server.js
@@ -76,6 +76,8 @@ res.json(body);
 
 });
 
+// delete todo id
+
 app.delete('/todos/:id', function (req, res){
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo=_.findWhere(todos, {id: todoId});
@@ -88,6 +90,42 @@ app.delete('/todos/:id', function (req, res){
 	}
 
 
+});
+
+
+app.put('/todos/:id', function(req, res){
+var todoId = parseInt(req.params.id, 10);
+var matchedTodo=_.findWhere(todos, {id: todoId});
+
+var body = _.pick(req.body, 'description', 'completed');
+var validAttributes= {}; 
+
+if(!matchedTodo){
+	return res.status(404).send();
+}
+
+
+if(body.hasOwnProperty('completed')&& _.isBoolean(body.completed)){
+  validAttributes.completed= body.completed;
+
+}else if(body.hasOwnProperty('completed')){
+   return res.status(400).send();
+}
+
+if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length>0){
+
+validAttributes.description= body.description;
+} else if(body.hasOwnProperty('description')){
+
+return res.status(400).send();
+
+}
+
+
+
+_.extend(matchedTodo, validAttributes);
+
+res.json(matchedTodo);
 });
 
 
