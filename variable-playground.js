8edee9056678d1fec7586c899977f20fbd6c9344
zakapong <zaka.pong@yahoo.com>
2016-07-19 10:50:16 +0200

    add playgound

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person= {
+// name: 'Andrew',
+// age: 21
+
+// };
+
+// function updatePerson(obj){
+
+// 	obj.age= 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// array example
+var grades = [15, 88];
+
+function addGrades(gradesArr){
+	gradesArr.push(55);
+
+	debugger;
+
+//gradesArr= [12, 33, 99];
+
+}
+
+addGrades(grades);
+console.log(grades);
